72cbc44c2cf54d2d1a2ba887f2a433af38c10d23
hemashrikumar-collab <hemashrikumar@gmail.com>
2026-02-08 11:54:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -180,7 +180,7 @@ export default function App() {
   );
 
   const questionText3 = useInfiniteTypewriter(
-    "Happy Valentine’s Day, My Love #RGForever 💖",
+    "Happy Valentine’s Day, My Chellakuttyy ummmmaaahhhhh💖",
     70,
     2000
   );
